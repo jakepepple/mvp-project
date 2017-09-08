95b1b34beea5605b73940fd143bcfb3bf524509d
Jake Pepple <jakepeppledrums@gmail.com>
2017-09-08 14:35:50 -0500

    connect to database

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mvpapp');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database successfully connected')
+})
+
 
 const app = express();
+app.use('/static', express.static(path.join(__dirname, 'client')))
 
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-  console.log(__dirname);
   res.sendFile(`${__dirname}/client/index.html`);
-  //res.send('Hello World');
 })
 
 app.listen(port, () => {
