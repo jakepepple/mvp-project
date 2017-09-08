7e4db715c5130a0dc39223efa3899341f09c1c5a
Jake Pepple <jakepeppledrums@gmail.com>
2017-09-08 12:12:35 -0500

    Serve index.html file

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  console.log(__dirname);
+  res.sendFile(`${__dirname}/client/index.html`);
+  //res.send('Hello World');
 })
 
 app.listen(port, () => {
